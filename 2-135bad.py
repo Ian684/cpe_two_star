7778d429187a3517dcd6b129e49aa3d633bbe351
ian684 <ian960224@gmail.com>
2026-03-11 12:41:57 +0800

    add some two star answers and adjust file name

--- 2-135bad.py
+++ 2-135bad.py
@@ -1,3 +1,4 @@
+import sys
 def generate_alph():
     alph = set()
     index = {}
@@ -21,30 +22,28 @@ def generate_alph():
     return alph , index
 def main():
     alph , index = generate_alph()
-    last = ''
+    line = sys.stdin.readlines()
+    line = ''.join(line)
+    l = 0
     while True:
-        flag = True
-        stop = True
         arr = []
-        while flag:
-            line = input()
-            line = last + line
-            for l in range(len(line)):
-                if line[l] == "#":
-                    last = line[l+1:]
-                    flag = False
-                    break
-                if line[l] in alph:
-                    stop = False 
-                    arr.append(line[l])
-        if stop or last == "#":
-            break
+        while True:
+            if line[l] == "#":
+                l += 1
+                break
+            if line[l] not in alph:
+                l += 1
+                continue
+            arr.append(line[l])
+            l += 1
+        if len(arr) == 0:break
         temp = ''
         for a in arr:
-            t = bin(index[a])[2:]
-            temp += (6-len(t))*'0'+t
+            temp +=  bin(index[a])[2:].zfill(6)
+        output_buffer = bytearray()
         for i in range(0 , len(temp) // 8 * 8 , 8):
-            print(chr(int(temp[i:i+8] , 2)),end="")
-        print("#",end="")
+            output_buffer.append(int(temp[i:i+8] , 2))
+        sys.stdout.buffer.write(output_buffer)
+        sys.stdout.buffer.write(b"#")
 if __name__ == "__main__":
     main()
